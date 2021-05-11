ae43cef8224e3019e148e5ae5ec66fdb7ea648aa
Xdargent <76215277+Xdargent@users.noreply.github.com>
2021-05-11 09:55:05 +0200

    1er update departement

--- Projet/src/Models/Departement.java
+++ Projet/src/Models/Departement.java
@@ -1,5 +1,18 @@
 package Models;
 
 public class Departement {
+	
+	private int idDep;
+	private String nameDep;
+	
+	
+	
+	public String getnameDep() {
+		return nameDep;
+	}
+	
+	public void setnameDep(String Sarg) {
+		nameDep = Sarg;
+	}
 
 }
