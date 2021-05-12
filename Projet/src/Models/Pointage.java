ceb837d907dd10e4f301488cbc59bd7718ccf6c5
Okaazkul <dylan.vinet29@gmail.com>
2021-05-12 15:20:47 +0200

    Début Pointage.java

--- Projet/src/Models/Pointage.java
+++ Projet/src/Models/Pointage.java
@@ -1,5 +1,42 @@
 package Models;
 
-public class Pointage {
+import java.time.LocalDateTime;
 
+public class Pointage {
+	private int idEmp;
+	private LocalDateTime time;
+	
+	public Pointage(int i) {
+		idEmp = i;
+		time = arrondiQuartHeure(LocalDateTime.now());
+	}
+	
+	public LocalDateTime arrondiQuartHeure(LocalDateTime t) {
+		int diff = t.getMinute()%15;
+		LocalDateTime tqh = null;
+		if(diff < 8) {
+			tqh = t.minusMinutes(diff);
+		}else if(diff >= 8) {
+			tqh = t.plusMinutes(15-diff);
+		}
+		tqh = tqh.minusSeconds(tqh.getSecond());
+		tqh = tqh.minusNanos(tqh.getNano());
+		return tqh;
+	}
+	
+	public LocalDateTime getTime() {
+		return time;
+	}
+	
+	public int getIdEmp() {
+		return idEmp;
+	}
+	
+	public String toString() {
+		return "Pointage de l'employé "+idEmp+" à "+time;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Pointage(0));
+	}
 }
