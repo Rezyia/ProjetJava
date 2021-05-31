b5de0d1ad14ee53e5676efa7bf564b43a9033ce2
Okaazkul <dylan.vinet29@gmail.com>
2021-05-31 11:35:57 +0200

    Intégration communication TCP entre émulateur et principale
    Modification des classes du packages Controller, basé sur les cours de système répartie, et ajout d'un nouveau constructeur de Pointage

--- Projet/src/Controlers/Emulateur.java
+++ Projet/src/Controlers/Emulateur.java
@@ -1,7 +1,35 @@
 package Controlers;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.time.LocalDateTime;
+
+import Models.Pointage;
 
 public class Emulateur {
 
-
+	Socket s = null;
+    InetSocketAddress isA = null;
+    
+    private void setSocket() throws IOException{
+    	isA = new InetSocketAddress("localhost", 8085);
+        s = new Socket(isA.getHostName(), isA.getPort());
+    }
+    
+    public void sendPointing(int idEmp, LocalDateTime time) {
+    	try{
+            System.out.println("TCPClientObjet launched...");
+            setSocket();
+            System.out.println("Hello, the client is connected");
+            Pointage pt = new Pointage(idEmp, time);
+            ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+            out.writeObject(pt);
+            out.flush();
+            out.close();
+            s.close();
+        }catch(IOException e){
+            System.out.println("IOException TCPClientObjet");
+        }
+    }
 }
--- Projet/src/Controlers/Principale.java
+++ Projet/src/Controlers/Principale.java
@@ -1,9 +1,11 @@
 package Controlers;
 
 import java.io.*;
+import java.net.*;
 import java.awt.*;
 import javax.swing.*;
 
+import Models.Pointage;
 import Views.FenetrePrincipale;
 import Views.WindowPointings;
 
@@ -14,6 +16,38 @@ import Views.WindowPointings;
 public class Principale {
 
 	private static FenetrePrincipale f = new FenetrePrincipale();
+	
+	Socket s = null;
+	ServerSocket ss = null;
+	InetSocketAddress isA = null;
+	
+	private void setSocket() throws IOException {
+        isA = new InetSocketAddress("localhost", 8085);
+        ss = new ServerSocket(isA.getPort());
+        s = ss.accept();
+    }
+	
+	/**
+	 * Méthode pour recevoir un objet de la classe Pointage via l'émulateur
+	 * @return Un pointeur crée et envoyé par l'émulateur
+	 */
+	public Pointage receivePointing() {
+		try{
+            System.out.println("TCPServerHello launched...");
+            setSocket();
+            System.out.println("Hello, the server is on");
+            ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+            Pointage pt = (Pointage) in.readObject();
+            //System.out.println(pt);
+            in.close();
+            s.close();
+            ss.close();
+            return pt;
+        }catch(Exception e){
+            System.out.println("Exception TCP : receivePointing()");
+        }
+		return null;
+	}
 	
 	/**
 	 * Main program
