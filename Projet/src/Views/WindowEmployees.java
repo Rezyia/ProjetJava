28c195b8c8eefcf9ff0de83aa3b122c38f0358ea
G-Delp <75621972+G-Delp@users.noreply.github.com>
2021-05-31 12:15:24 +0200

    Create WindowEmployees.java

--- Projet/src/Views/WindowEmployees.java
+++ Projet/src/Views/WindowEmployees.java
@@ -1,4 +1,4 @@
-package Views;
+package views;
 
 import javax.swing.*;
 
@@ -10,5 +10,37 @@ import java.awt.*;
 import javax.swing.*;
 
 public class WindowEmployees{
+  // Attributes :
+	private static int defaultWidth = 720;
+	private static int defaultHeight = 480;
+	
+	private static int defaultLabelHeight = 30;
+	
+	private DefaultListModel<String> dlmEmployees;
+  
+  public WindowEmployees() {
+		panel = new JPanel();
+		panel.setBounds(0, 40, defaultWidth, defaultHeight);
 
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		
+		dlmEmployees = new DefaultListModel<String>();
+		
+		panel.add(createList(dlmEmployees));
+    panel.add(createEmployeeDetails());
+  }
+  
+  public WindowEmployee(JFrame frame){
+    panel = new JPanel();
+		panel.setBounds(0, 40, frame.getWidth(), frame.getHeight()-FenetrePrincipale.headerHeight);
+	
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		panel.setBackground(new Color(120, 120, 120));
+		
+		panel.add(createList(employees));
+		panel.add(createEmployeeDetails());
+		
+		System.out.println(panel.getWidth() + " - " + panel.getHeight());
+
+	}
 }
