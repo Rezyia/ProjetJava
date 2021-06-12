e5c4b1cde83f2aa44704be7d30dd8e646e4ec2f6
Rezyia <fineladiesprotectress@gmail.com>
2021-06-12 16:31:03 +0200

    update doc

--- Projet/doc/package-search-index.js
+++ Projet/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Views"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Models"},{"l":"Views"}];updateSearchResults();
